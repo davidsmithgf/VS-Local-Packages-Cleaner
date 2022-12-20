bb97a905c4d18db10e5f651c9052ac0637c39855
gaofeng <gaofeng@icicle.com>
2022-12-20 15:08:25 +0800

    update for using Catelog.json

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ using System.Data.SQLite;
 using Dapper;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using VS_Local_Packages_Cleaner.CatalogJson;
 
 namespace VS_Local_Packages_Cleaner
 {
@@ -16,6 +19,7 @@ namespace VS_Local_Packages_Cleaner
         static void Main(string[] args)
         {
             var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            bool readCatalogJson = Convert.ToBoolean(config.GetSection("AppSettings")["readCatalogJson"]);
             string connString = config.GetConnectionString("sqlite");
             bool resetVerNum = Convert.ToBoolean(config.GetSection("AppSettings")["resetVerNum"]);
             string strDropTableSql = config.GetSection("AppSettings")["sqliteDropTable"];
@@ -26,10 +30,11 @@ namespace VS_Local_Packages_Cleaner
                 config.GetSection("AppSettings")["sqliteUpdate4"] +
                 config.GetSection("AppSettings")["sqliteUpdate5"] +
                 config.GetSection("AppSettings")["sqliteUpdate6"];
+            string strUpdateSql223 = config.GetSection("AppSettings")["sqliteUpdate1"];
             string strQuerySql = config.GetSection("AppSettings")["sqliteQuery"];
             DateTime newfileTimeline = Convert.ToDateTime(config.GetSection("AppSettings")["newfileTimeline"]);
 
-            Console.WriteLine("Local Visual Studio installation full path: ");
+            Console.WriteLine("Local Visual Studio installation package full path: ");
             var dirPackages = new DirectoryInfo(Console.ReadLine().Trim());
             if (!dirPackages.Exists)
             {
@@ -38,6 +43,7 @@ namespace VS_Local_Packages_Cleaner
             else
             {
                 var _fileName = dirPackages.Name + DateTime.Now.ToString("yyyyMMddHHmmss");
+                connString = connString.Replace("[PackagesFileName]", _fileName);
 
                 var dtPackages = new DataTable("Packages");
                 dtPackages.Columns.Add(new DataColumn("DirectoryName", typeof(string)));
@@ -49,11 +55,9 @@ namespace VS_Local_Packages_Cleaner
                 // 1) Get all package list for operation
                 foreach (var pkg in dirPackages.GetDirectories())
                 {
+                    if (pkg.Name == "certificates" || pkg.Name == "Archive")
+                        continue;
                     //if (!pkg.Name.Contains("Microsoft.AspNetCore.SharedFramework.3"))
-                    //    continue; // for debug only, comment the line before release
-                    //if (!pkg.Name.Contains("Microsoft.AspNetCore.TargetingPack"))
-                    //    continue; // for debug only, comment the line before release
-                    //if (!pkg.Name.Contains("Microsoft.VC"))
                     //    continue; // for debug only, comment the line before release
 
                     var pkgName = (pkg.Name.IndexOf(",") > 0) ? (pkg.Name.Substring(0, pkg.Name.IndexOf(","))) : (pkg.Name);
@@ -61,9 +65,10 @@ namespace VS_Local_Packages_Cleaner
                     if (pkg.Name.ToLower().Contains("x64") && !pkgName.ToLower().Contains("x64")) pkgName += ".x64";
                     if (pkg.Name.ToLower().Contains("arm64") && !pkgName.ToLower().Contains("arm64")) pkgName += ".arm64";
                     if (pkg.Name.ToLower().Contains("en-us") && !pkgName.ToLower().Contains("en-us")) pkgName += ".en-us";
-                    if (pkg.Name.ToLower().Contains("zh-cn") && !pkgName.ToLower().Contains("arm64")) pkgName += ".zh-cn";
+                    if (pkg.Name.ToLower().Contains("zh-cn") && !pkgName.ToLower().Contains("zh-cn")) pkgName += ".zh-cn";
 
                     var majorVer = Convert.ToInt32(config.GetSection("AppSettings")["majorVer"]);
+                    if (pkg.Name.Contains("Microsoft.iOS")) majorVer = 1;
                     if (pkg.Name.Contains("Microsoft.Net.")) majorVer = 3;
                     if (pkg.Name.Contains("Microsoft.VC.")) majorVer = 4;
                     if (pkg.Name.Contains("Win10_")) majorVer = 3;
@@ -81,8 +86,48 @@ namespace VS_Local_Packages_Cleaner
                     Console.WriteLine(dtPackages.Rows.Count + "\t" + dr[0] + "\t" + dr[3]);
                 }
 
-                // 2) Save package table to sqlite;
-                var toDeleteDirectoryNames = DealDataWithSqlite(dtPackages, newfileTimeline, connString.Replace("[PackagesFileName]", _fileName), strDropTableSql, strCreateTableSql, strUpdateSql, strQuerySql).Result;
+                // 2.1) Save package table to sqlite;
+                var toDeleteDirectoryNames = DealDataWithSqlite(dtPackages, newfileTimeline, connString, strDropTableSql, strCreateTableSql, strUpdateSql, strQuerySql).Result;
+
+                // 2.2) Compare directories in Catalog.Json with the directories in installation path
+                if (readCatalogJson)
+                {
+                    // 2.2.1) Find directories in Catalog.json
+                    var allDirectories = new Dictionary<string, VSPackage>();
+                    var fileCatalogJson = dirPackages + "\\Catalog.json";
+                    if (File.Exists(fileCatalogJson))
+                    {
+                        var objCatalogJson = JObject.Parse(File.ReadAllText(fileCatalogJson, Encoding.UTF8));
+                        foreach (var p in JsonConvert.DeserializeObject<List<VSPackage>>(objCatalogJson["packages"].ToString()))
+                            if (!allDirectories.ContainsKey(p.ToString()))
+                                allDirectories.Add(p.ToString().ToLower(), p);
+
+                        // 2.2.2) Mark to delete directories if not exist in Catalog.json
+                        var notContainDirectories = new List<string>();
+                        var strDelDirForSqlite = new StringBuilder();
+                        foreach (DataRow dr in dtPackages.Rows)
+                            if (!allDirectories.ContainsKey(dr["DirectoryName"].ToString().ToLower()))
+                            {
+                                notContainDirectories.Add(dr["DirectoryName"].ToString());
+                                if (!toDeleteDirectoryNames.Contains(dr["DirectoryName"].ToString()))
+                                {
+                                    toDeleteDirectoryNames.Add(dr["DirectoryName"].ToString());
+                                    if (strDelDirForSqlite.Length > 0)
+                                        strDelDirForSqlite.Append(",\"" + dr["DirectoryName"].ToString() + "\"");
+                                    else
+                                        strDelDirForSqlite.Append("\"" + dr["DirectoryName"].ToString() + "\"");
+                                }
+                            }
+
+                        // 2.2.3) Update sqlite with mark "ToDelete"
+                        if (strDelDirForSqlite.Length > 0)
+                            UpdateDataWithSqlite(connString, strUpdateSql223 + "(" + strDelDirForSqlite.ToString() + ");");
+                    }
+                    else
+                    {
+                        Console.WriteLine("[Bypassing] Could not find Catalog.json file in " + dirPackages);
+                    }
+                }
 
                 // 3) Create clean script for directory marked as "ToDelete"
                 CreateCleanerScript(Directory.GetCurrentDirectory(), _fileName + ".cmd", dirPackages.FullName, toDeleteDirectoryNames);
@@ -90,6 +135,7 @@ namespace VS_Local_Packages_Cleaner
                 // new DirectoryInfo(dirPackages.Root + "$RECYCLE.BIN").GetDirectories()
                 // pending for using first solution
             }
+            Console.WriteLine("New File Timeline: " + newfileTimeline.ToString("yyyy-MM-dd HH:mm:ss"));
             Console.WriteLine("Press any key to exit ...");
             Console.ReadKey();
         }
@@ -156,13 +202,13 @@ namespace VS_Local_Packages_Cleaner
                 {
                     conn.Open();
 
-                    // 2.1) Drop table if exists
+                    // 2.1.1) Drop table if exists
                     conn.Execute(strDropTableSql);
 
-                    // 2.2) Create an empty table
+                    // 2.1.2) Create an empty table
                     conn.Execute(strCreateTableSql);
 
-                    // 2.3) Loop and insert, save all packages into sqlite;
+                    // 2.1.3) Loop and insert, save all packages into sqlite;
                     foreach (DataRow r in dtSource.Rows)
                         conn.Execute("INSERT INTO Packages(DirectoryName, PackageName, PackageNameNoVer, LastWriteTime, ToDelete) VALUES('" +
                             r["DirectoryName"].ToString() + "', '" +
@@ -171,10 +217,10 @@ namespace VS_Local_Packages_Cleaner
                             Convert.ToDateTime(r["LastWriteTime"]).ToString("yyyy-MM-dd HH:mm:ss") + "', '" +
                             Convert.ToBoolean(r["ToDelete"]).ToString() + "');");
 
-                    // 2.4) Query & update which package should be marked as "ToDelete";
+                    // 2.1.4) Query & update which package should be marked as "ToDelete";
                     await conn.ExecuteAsync(strUpdateSql.Replace("yyyy-MM-dd HH:mm:ss", newfileTimeline.ToString("yyyy-MM-dd HH:mm:ss")));
 
-                    // 2.5) Query and sync data back, from sqlite file to DataTable
+                    // 2.1.5) Query and sync data back, from sqlite file to DataTable
                     var toDeleteDirectoryNames = new Dictionary<string, bool>();
                     var toDeleteReader = conn.ExecuteReader(strQuerySql);
                     while (toDeleteReader.Read())
@@ -189,6 +235,32 @@ namespace VS_Local_Packages_Cleaner
                 {
                     Console.WriteLine(ex.ToString());
                     return null;
+                }
+                finally
+                {
+                    if (conn != null)
+                    {
+                        conn.Close();
+                        conn.Dispose();
+                    }
+                }
+            }
+        }
+
+        private static int UpdateDataWithSqlite(string connString, string strUpdateSql)
+        {
+            using (var conn = new SQLiteConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    return conn.Execute(strUpdateSql);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return -1;
                 }
                 finally
                 {
---
+++ VSPackage.cs
@@ -0,0 +1,147 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VS_Local_Packages_Cleaner.CatalogJson
+{
+    public class VSPackage
+    {
+        public string id { get; set; }
+        public string version { get; set; }
+        public string type { get; set; }
+        public Dependencies dependencies { get; set; }
+        public string language { get; set; }
+        public List<LocalizedResource> localizedResources { get; set; }
+        public string chip { get; set; }
+        public string productArch { get; set; }
+        public List<Payload> payloads { get; set; }
+        public MsiProperties msiProperties { get; set; }
+        public string productCode { get; set; }
+        public string upgradeCode { get; set; }
+        public string productVersion { get; set; }
+        public int? productLanguage { get; set; }
+        public string providerKey { get; set; }
+        public InstallSizes installSizes { get; set; }
+        public List<LogFile> logFiles { get; set; }
+        public InstallParams installParams { get; set; }
+        public RepairParams repairParams { get; set; }
+        public UninstallParams uninstallParams { get; set; }
+        public string relativePath { get; set; }
+        public DetectConditions detectConditions { get; set; }
+        public string vsixId { get; set; }
+        public string extensionDir { get; set; }
+        public Requirements requirements { get; set; }
+        public string license { get; set; }
+        public bool? isUiGroup { get; set; }
+        public string machineArch { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(id);
+            if (!string.IsNullOrWhiteSpace(version))
+                sb.Append(",version=" + version);
+            if (!string.IsNullOrWhiteSpace(chip))
+                sb.Append(",chip=" + chip);
+            if (!string.IsNullOrWhiteSpace(language))
+                sb.Append(",language=" + language);
+            if (!string.IsNullOrWhiteSpace(productArch))
+                sb.Append(",productarch=" + productArch);
+            if (!string.IsNullOrWhiteSpace(machineArch))
+                sb.Append(",machinearch=" + machineArch);
+            return sb.ToString();
+        }
+    }
+
+    public class Dependencies
+    {
+
+    }
+
+    public class LocalizedResource
+    {
+        public string language { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public List<string> keywords { get; set; }
+    }
+
+    public class Payload
+    {
+        public string fileName { get; set; }
+        public string sha256 { get; set; }
+        public int size { get; set; }
+        public string url { get; set; }
+        public Signer signer { get; set; }
+        public bool? isDynamicEndpoint { get; set; }
+    }
+
+    public class Signer
+    {
+        [JsonProperty("$id")]
+        public string id { get; set; }
+        public string subjectName { get; set; }
+    }
+
+    public class MsiProperties
+    {
+        public string MSIFASTINSTALL { get; set; }
+        public string VSEXTUI { get; set; }
+    }
+
+    public class InstallSizes
+    {
+        public long systemDrive { get; set; }
+        public long? sharedDrive { get; set; }
+        public long? targetDrive { get; set; }
+    }
+
+    public class LogFile
+    {
+        public string pattern { get; set; }
+    }
+
+    public class InstallParams
+    {
+        public string fileName { get; set; }
+        public string parameters { get; set; }
+    }
+    public class UninstallParams
+    {
+        public string fileName { get; set; }
+        public string parameters { get; set; }
+    }
+    public class RepairParams
+    {
+        public string fileName { get; set; }
+        public string parameters { get; set; }
+    }
+
+    public class DetectConditions
+    {
+        public List<Condition> conditions { get; set; }
+        public string expression { get; set; }
+    }
+    public class Condition
+    {
+        public string filePath { get; set; }
+        public string registryKey { get; set; }
+        public string id { get; set; }
+        public string registryValue { get; set; }
+        public string join { get; set; }
+        public string registryData { get; set; }
+    }
+
+    public class Requirements
+    {
+        public Functors functors { get; set; }
+        public string supportedOS { get; set; }
+    }
+
+    public class Functors
+    {
+        public string architecture { get; set; }
+    }
+}
